0f046fe651042148624229d1692e905a438c5d9f
Tres18 <vincentjohntresreyes@gmail.com>
2024-10-01 17:23:23 +0800

    Changed the db connection

--- dbconnection.php
+++ dbconnection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";  // Usually 'localhost' when using XAMPP
 $username = "root";         // Default username for XAMPP
 $password = "";             // Default password is empty for XAMPP
-$dbname = "uvm_new";  // Replace with your actual database name
+$dbname = "uvm_db";  // Replace with your actual database name
 
 // Create connection
 $conne = new mysqli($servername, $username, $password, $dbname);
