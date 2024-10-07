148f3aeac09faed7e72ec5a42afac1cde385c44c
Tres18 <vincentjohntresreyes@gmail.com>
2024-10-07 11:37:03 +0800

    updated purchase history table column names and added status.

--- main-content/PurchaseHistory.php
+++ main-content/PurchaseHistory.php
@@ -13,34 +13,34 @@ $display_purchase_history = mysqli_query($conne, $query);
 
 <div class="download-purchase-history">
 
-        <input type="button" class="download-data" value="Export .csv" onclick="window.location.href='download_data.php'">
+    <input type="button" class="download-data" value="Export .csv" onclick="window.location.href='download_data.php'">
     </form>
 </div>
 <div class="transactions-table-container">
     <table>
         <tr>
-            <th>transaction_ID</th>
-            <th>order_ID</th>
-            <th>user_ID</th>
-            <th>total_quantity</th>
-            <th>total_amount</th>
-            <th>transaction_date</th>
-            <th>qr_code</th>
+            <th>Transaction ID</th>
+            <th>Order ID</th>
+            <th>Total Quantity</th>
+            <th>Total Amount</th>
+            <th>Transaction Date</th>
+            <th>QR Code</th>
+            <th>Status</th>
         </tr>
         <tr>
             <?php while ($row = mysqli_fetch_assoc($display_purchase_history)) { ?>
-        <tr>
-            <td><?php echo htmlspecialchars($row['transaction_id']); ?></td>
-            <td><?php echo htmlspecialchars($row['order_id']); ?></td>
-            <td><?php echo htmlspecialchars($row['user_id']); ?></td>
-            <td><?php echo htmlspecialchars($row['total_quantity']); ?></td>
-            <td><?php echo htmlspecialchars($row['total_amount']); ?></td>
-            <td><?php echo htmlspecialchars($row['transaction_date']); ?></td>
-            <td><?php echo htmlspecialchars($row['qr_code']); ?></td>
+            <tr>
+                <td><?php echo htmlspecialchars($row['transaction_id']); ?></td>
+                <td><?php echo htmlspecialchars($row['order_id']); ?></td>
+                <td><?php echo htmlspecialchars($row['total_quantity']); ?></td>
+                <td><?php echo htmlspecialchars($row['total_amount']); ?></td>
+                <td><?php echo htmlspecialchars($row['transaction_date']); ?></td>
+                <td><?php echo htmlspecialchars($row['qr_code']); ?></td>
+                <td><?php echo htmlspecialchars($row['status']); ?></td>
 
-        </tr>
-    <?php
+            </tr>
+            <?php
             }
-    ?>
+            ?>
     </table>
 </div>
